2ec49b98508103687ec808cb6c6d98e6d5f5d6cd
santoslopez <santos.lopez@galileo.edu.gt>
2021-10-03 08:35:54 +0000

    Enlace corregido webservices

--- index.php
+++ index.php
@@ -132,7 +132,7 @@
             <div class="alert alert-primary" role="alert">
               
                 <h2><span style="color:blue">Webservices</span></h2>
-                <a href="vuelosW/script_lista_vuelos.php">Ir script_lista_vuelos</a><br>
+                <a href="asientosW/script_lista_asientos.php">Ir script_lista_vuelos</a><br>
                 <a href="boletosW/script_reserva.php">Ir script_reserva</a><br>
                 <a href="vuelosW/script_lista_vuelos.php">Ir script_lista_vuelos</a><br>
 
