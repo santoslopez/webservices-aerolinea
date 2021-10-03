c34c1e5e2f0f1864a97f219d808b55a26931cf55
santoslopez <santos.lopez@galileo.edu.gt>
2021-10-03 07:31:05 +0000

    XML y JSON: script de boleto generado, sin embargo la consulta está mal hecha, solo falta corregirlo. Versión final para entregar.

---
+++ boletoW/script_reserva.php
@@ -0,0 +1,165 @@
+<?php 
+
+include '../conexion/conexion.php';
+
+$aerolinea = $_GET['aerolinea'];
+$asiento = $_GET['asiento'];
+$vuelo = $_GET['vuelo'];
+
+
+$fechaA = $_GET['fecha'];
+
+$nombre = $_GET['nombre']; 
+
+$formatoResultado = $_GET['formato'];
+
+
+$year = substr($fechaA,0,4);
+$month = substr($fechaA,4,2);
+$day = substr($fechaA,6,2);
+
+$fechaIng = "$year-$month-$day";
+
+$listadoVuelos ="SELECT matricula FROM Aeronave WHERE matricula='$aerolinea'";
+
+
+$infoVuelos  = "SELECT aer.matricula as aerolinea,vi.fecha as fecha,rut.horaSalida as hora,bol.numeroBoleto AS numero FROM Viaje as vi,Aeronave AS aer, Boletos AS bol, Rutas as rut WHERE vi.codigoViaje=$vuelo
+                AND bol.numeroBoleto=$vuelo AND aer.matricula='$aerolinea' AND  vi.fecha='$fechaIng'";
+
+//**
+ 
+//$infoVuelos ="SELECT aer.matricula as aerolinea,vi.fecha as fecha,rut.horaSalida as hora,bol.numeroBoleto AS numero FROM Viaje as vi,Aeronave AS aer, Boletos AS bol, Rutas as rut WHERE vi.codigoViaje=9
+//AND bol.numeroBoleto=9 AND aer.matricula='1' AND  vi.fecha='2021-10-10'"; 
+//*/
+
+
+
+$ejecutarConsultaObtenerInfo = pg_query($conexion,$listadoVuelos);
+$consultaInfoVuelos = pg_query($conexion, $infoVuelos);
+
+function resultadosJSON($ejecutarConsultaObtenerInfo,$consultaInfoVuelos){
+            // verificamos que existen registros, sino no dibujamos la tabla
+            if (!(pg_num_rows($ejecutarConsultaObtenerInfo))) {
+                $arrayDatosConsulta = array(); 
+                //header('Access-Control-Allow-Origin: *');
+                header('Content-Type: application/json');
+                $arrayDatosConsulta[] = array("Mensaje"=>"No hay informacion");
+                //Creamos el JSON
+                $json_string = json_encode($arrayDatosConsulta);
+                echo $json_string; 
+            }else{
+                $arrayDatosConsulta = array(); 
+
+                //echo '<lista_vuelos>';
+                //echo "\t<aerolinea>EY</aerolinea>\n";
+                    while ($row= pg_fetch_row($ejecutarConsultaObtenerInfo)) {
+                        $aerolinea = $row[0];
+
+                      
+                        $fechaISO = str_replace('-','',$fecha);
+
+                    
+                        $arreglo = array ("aerolinea"=>$aerolinea);
+                      
+                        if (!(pg_num_rows($consultaInfoVuelos))) {
+                            echo '<boletos>No hay informacion</boletos>';
+                        }else{
+                            while ($row= pg_fetch_row($consultaInfoVuelos)) {  
+                                //$vuelo = $row[0];
+   
+                                $fecha = $row[1];
+                                $hora= $row[2];
+                                //$numer= $row[2];
+                                $vuelo= $row[3];
+
+                                $horaISO = str_replace(':', '', DateTime::createFromFormat('H:i:s',$hora)->format('H:i'));  
+                                $arrayDatosConsulta = array("vuelo"=>$vuelo,"fecha"=>$fecha,"hora"=>$hora,"numero"=>$vuelo);                    
+                               
+                            }
+                          
+                           
+                        }
+                    }
+                    
+                    header('Content-Type: application/json');
+                    $json_string = json_encode($arreglo + $arrayDatosConsulta);
+                    echo $json_string; 
+                   
+            }
+}
+
+function resultadosXML($ejecutarConsultaObtenerInfo,$consultaInfoVuelos){
+        // verificamos que existen registros, sino no dibujamos la tabla
+        if (!(pg_num_rows($ejecutarConsultaObtenerInfo))) {
+            echo '<boleto>No hay informacion</boleto>';
+        }else{
+            echo '<boleto>';
+            //echo "\t<aerolinea>EY</aerolinea>\n";
+                while ($row= pg_fetch_row($ejecutarConsultaObtenerInfo)) {
+                    $aerolinea = $row[0];
+
+        
+                    $fechaISO = str_replace('-','',$fecha);
+                    echo "\t<aerolinea>$aerolinea</aerolinea>\n";
+                
+        
+                    if (!(pg_num_rows($consultaInfoVuelos))) {
+                        echo 'No hay informacion de viajes';
+                    }else{
+                        while ($row= pg_fetch_row($consultaInfoVuelos)) {     
+                            //$aerolinea = $row[0];
+                            $fecha= $row[1];
+                            $hora= $row[2];
+                            $numero= $row[3];
+
+                            $horaISO = str_replace(':', '', DateTime::createFromFormat('H:i:s',$hora)->format('H:i'));  
+    
+                            echo "\t\t<vuelo>$numero</vuelo>\n";
+                            echo "\t\t<fecha>$fecha</fecha>\n";
+                            echo "\t\t<hora>$horaISO</hora>\n";
+                            echo "\t\t<numero>$numero</numero>\n";
+
+                        }
+                    }
+                }
+            echo "</boleto>";
+        }
+}
+
+
+
+
+//Se permite que se ingrese 4 parametros o 5 con el formato
+if (($aerolinea  && $fechaA && $vuelo && $asiento && $nombre)  || ($aerolinea  && $fechaA && $vuelo && $asiento && $nombre && $formatoResultado)) {
+    
+    //Verificamos si existe un cuarto parametro
+    if (( empty ($formatoResultado ) ? NULL : $formatoResultado)) {
+        //establecer formatos
+        $formatoXML = "XML";
+        $formatoJSON = "JSON";
+
+        # Se verifica que el formato es XML del cuarto parametro
+        if (strcmp($formatoResultado, $formatoXML) === 0){
+            resultadosXML($ejecutarConsultaObtenerInfo,$consultaInfoVuelos);
+        # Se verifica que el formato es JSON del cuarto parametro
+        }else  if (strcmp($formatoResultado, $formatoJSON) === 0){
+            resultadosJSON($ejecutarConsultaObtenerInfo,$consultaInfoVuelos);
+        }else {
+            # code...
+            ECHO "El formato ingresado $formatoResultado no se reconoce";
+            ECHO "Esperaba aerolina=GU&vuelo=123&fecha=20210824&asiento=1A&nombre=JuanPerez&formato=NombreFormato";
+        }
+
+    }else {
+        // no se agrego ningun formato por default es XML
+        resultadosXML($ejecutarConsultaObtenerInfo,$consultaInfoVuelos);
+    }
+
+}else {
+    echo "Parametros no esperados";
+}
+
+
+
+?> 
+
--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
       integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
+      
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
       crossorigin="anonymous"></script>
@@ -23,8 +23,8 @@
 
 
     <!-- Sweet Alert2 personalizado para no usar mensajes javascript sin personalizar --->
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>  
+    
     <!-- Por medio de este archivo mostramos un mensaje de confirmacion para eliminar, actualizar datos.-->
     <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
 
@@ -37,15 +37,15 @@
 
     <main class="main" style="width:95%;margin-right:20%;">
         <!--div id="contents" style="background: red;width:90%;margin-right:10%;"-->
-
+ 
         <div class="tab-content" id="v-pills-tabContent" style="width:90%;margin-right:10%;margin-top:40px;">
         <div class="tab-pane fade" id="idiomas" role="tabpanel" aria-labelledby="idiomas-tab" style="width:100%">
-
+        
             <div class="alert alert-primary" role="alert">
                 <h3><span>Listado idiomas</span> </h3>
-            </div>
-            <?php
-                include 'idiomas/listadoIdiomas.php';
+            </div>            
+            <?php        
+                include 'idiomas/listadoIdiomas.php';    
             ?>
         </div>
         <div class="tab-pane fade" id="empleados" role="tabpanel" aria-labelledby="empleados-tab" style="width:100%">
@@ -53,28 +53,28 @@
                 <h2><span style="color:blue">Empleados</span></h2>
             </div>
             <br>
-            <?php
-                include 'empleados/listadoEmpleados.php';
+            <?php           
+                include 'empleados/listadoEmpleados.php';    
             ?>
         </div>
-
+    
         <div class="tab-pane fade" id="empleadosIdiomas" role="tabpanel" aria-labelledby="empleadosIdiomas-tab" style="width:100%">
             <div class="alert alert-primary" role="alert">
                 <h2><span style="color:blue">Empleados idiomas</span></h2>
             </div>
             <br>
             <?php
-                include 'idiomasEmpleados/listadoEmpleadosIdiomas.php';
+                include 'idiomasEmpleados/listadoEmpleadosIdiomas.php';    
             ?>
         </div>
-
+    
 
         <div class="tab-pane fade" id="aeropuerto" role="tabpanel" aria-labelledby="aeropuerto-tab" style="width:100%">
             <div class="alert alert-primary" role="alert">
                 <h2><span style="color:blue">Aeropuerto</span></h2>
             </div>
-            <?php
-                include 'aeropuerto/listadoAeropuerto.php';
+            <?php       
+                include 'aeropuerto/listadoAeropuerto.php';    
             ?>
         </div>
 
@@ -82,18 +82,18 @@
             <div class="alert alert-primary" role="alert">
                 <h2><span style="color:blue">Rutas</span></h2>
             </div>
-            <?php
-                include 'rutas/listadoRutas.php';
+            <?php            
+                include 'rutas/listadoRutas.php';    
             ?>
 
         </div>
 
-        <div class="tab-pane fade" id="viajes" role="tabpanel" aria-labelledby="viajes-tab" style="idth:100%">
+        <div class="tab-pane fade" id="viajes" role="tabpanel" aria-labelledby="viajes-tab" style="width:100%">
             <div class="alert alert-primary" role="alert">
                 <h2><span style="color:blue">Viajes</span></h2>
             </div>
-            <?php
-                include 'viajes/listadoViajes.php';
+            <?php              
+                include 'viajes/listadoViajes.php';    
             ?>
         </div>
 
@@ -101,8 +101,8 @@
             <div class="alert alert-primary" role="alert">
                 <h2><span style="color:blue">Viajes por Empleado</span></h2>
             </div>
-            <?php
-                include 'viajesEmpleados/listadoEmpleadosViajes.php';
+            <?php              
+                include 'viajesEmpleados/listadoEmpleadosViajes.php';    
             ?>
         </div>
 
@@ -111,34 +111,32 @@
             <div class="alert alert-primary" role="alert">
                 <h2><span style="color:blue">Aerolineas</span></h2>
             </div>
-            <?php
-                include 'aeronave/listadoAeronave.php';
+            <?php         
+                include 'aeronave/listadoAeronave.php';    
             ?>
         </div>
 
 
         <div class="tab-pane fade" id="boletosAereos" role="tabpanel" aria-labelledby="boletosAereos-tab" style="width:100%">
             <div class="alert alert-primary" role="alert">
-
+              
                 <h2><span style="color:blue">Boletos Aéreos</span></h2>
             </div>
-            <?php
-                include 'boletos/listadoBoletos.php';
+            <?php                    
+                include 'boletos/listadoBoletos.php';    
             ?>
-
+            
         </div>
 
         <div class="tab-pane fade" id="webservices" role="tabpanel" aria-labelledby="webservices-tab" style="width:100%">
             <div class="alert alert-primary" role="alert">
-
+              
                 <h2><span style="color:blue">Webservices</span></h2>
 
                 <a href="vuelosW/script_lista_vuelos.php">Ir script_lista_vuelos</a><br>
-                <!--a href="vuelosW/ver_xml.php">XML lista VUELOS</a><br-->
-                <a href="asientosW/script_lista_asientos.php">Ir script lista vuelos</a><br>
-
+                <a href="vuelosW/ver_xml.php">XML lista VUELOS</a><br>
             </div>
-
+           
         </div>
 
 
@@ -165,9 +163,9 @@
     </div>
         <!--/div-->
     </main>
-
-
-
+   
+   
+ 
     <header class="header" id="header">
         <div>
             <i class='bx bx-menu' id="header-toggle"></i>
@@ -180,7 +178,7 @@
 
     <div class="l-navbar" id="nav-bar">
         <nav class="nav">
-            <div id="v-pills-tab">
+            <div id="v-pills-tab">  
                 <a href="#" class="nav__logo">
                     <i class='bx bxs-plane-take-off nav__logo-icon'></i>
                     <!--i class='bx bxs-plane-take-off nav__logo-icon'></i-->
@@ -190,7 +188,7 @@
                 </a>
 
 
-
+                
                 <div  class="nav nav-tabs nav__list" id="nav-tab" role="tablist">
 
                     <!-- Acerca -->
@@ -199,7 +197,7 @@
                             <span class="nav__name">
                                 Acerca
                             </span>
-                    </a>
+                    </a> 
                     <!-- AERONAVE -->
                     <a class="nav-link" id="aeronave-tab" data-toggle="pill" href="#aeronave"  role="tab" aria-controls="aeronave" aria-selected="false">
                         <i class='bx bxs-plane nav__icon'></i>
@@ -208,7 +206,7 @@
                             Aeronave
                         </span>
                     </a>
-
+                    
                     <!-- AEROPUERTO -->
                     <a class="nav-link" id="aeropuerto-tab" data-toggle="pill" href="#aeropuerto"  role="tab" aria-controls="aeropuerto" aria-selected="false">
                         <i class='bx bxs-plane-take-off nav__icon'></i>
@@ -220,33 +218,33 @@
 
                 <!-- Idiomas -->
 
-                <a class="nav-link" id="idiomas-tab" data-toggle="pill" href="#idiomas"  role="tab" aria-controls="idiomas" aria-selected="false" >
-                    <i class='bx bxs-flag-alt nav__icon'></i>
+                <a class="nav-link" id="idiomas-tab" data-toggle="pill" href="#idiomas"  role="tab" aria-controls="idiomas" aria-selected="false" >            
+                    <i class='bx bxs-flag-alt nav__icon'></i>       
                     <span class="nav__name">
                         Idiomas
                     </span>
                 </a>
-
+  
                  <!-- Empleados -->
                     <!--a href="#v-pills-profile" class="nav-link nav__link" role="tab" aria-controls="v-pills-profile" aria-selected="false"--->
                     <a class="nav-link" id="empleados-tab" data-toggle="pill" href="#empleados"  role="tab" aria-controls="empleados" aria-selected="false">
-
+                       
                     <i class='bx bxs-user-circle nav__icon'></i>
                         <span class="nav__name">
                             Empleados
                         </span>
-                    </a>
+                    </a>  
 
                     <!-- IDIOMAS QUE DOMINA EMPLEADO -->
                     <a class="nav-link" id="empleadosIdiomas-tab" data-toggle="pill" href="#empleadosIdiomas"  role="tab" aria-controls="empleadosIdiomas" aria-selected="false">
-
+                       
                        <i class='bx bxs-user-circle nav__icon'></i>
                            <span class="nav__name">
-                               Idiomas  empleado
+                               Idiomas  empleado 
                            </span>
-                       </a>
+                       </a>  
 
-
+                    
                     <!-- RUTAS-->
                     <a class="nav-link" id="rutas-tab" data-toggle="pill" href="#rutas"  role="tab" aria-controls="rutas" aria-selected="false">
                         <i class='bx bxs-map nav__icon'></i>
@@ -291,18 +289,18 @@
                            Webservices
                         </span>
                     </a>
-
-
-
+ 
+                    
+ 
                 </div>
             </div>
 
             <a href="#" class="nav__link">
                 <i class='bx bx-info-circle nav__icon'></i>
                 <span class="nav__name">
-                    CC6
+                    CC6 
                 </span>
-            </a>
+            </a>  
 
         </nav>
         <script src="js/main.js"></script>
@@ -318,8 +316,8 @@
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    
 
-
-
+    
 </body>
 </html>
