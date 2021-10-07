ea98ae65c90edc37c0a989070c0a6e069e5efa5b
Santos López <santos.lopez@galileo.edu.gt>
2021-10-07 17:35:42 -0600

    Script finalizado. Funcionan con los otros sistema
    Script finalizados, pruebas realizadas con los demás sistemas.

--- script_reserva.php
+++ script_reserva.php
@@ -17,71 +17,140 @@ $day = substr($fechaA,6,2);
 
 $fechaIng = "$year-$month-$day";
 
-$posicion = substr($asiento, -1);
+$posicionA = substr($asiento, -1);
 
 if(strlen($asiento) == 3){
-    $fila = substr($asiento, 0, 2);
+    $filaA = substr($asiento, 0, 2);
 }else if(strlen($asiento) == 2){
-    $fila = substr($asiento, 0, 1);
+    $filaA = substr($asiento, 0, 1);
 }
 
-$infoVuelos  = "SELECT V.numeroVuelo, V.fecha, R.horaSalida, B.numeroBoleto
-                FROM Boletos as B, Viaje AS V, Rutas AS R
-                WHERE V.codigoViaje = B.codigoViaje AND R.numeroVuelo = V.numeroVuelo AND  V.numeroVuelo = $vuelo AND B.fila = $fila AND B.posicion = '$posicion' ";
+$consultaAsientoOcupado = " SELECT V.codigoViaje, V.numeroVuelo, V.fecha, B.fila, B.posicion, B.nombrePasajero
+                            FROM Boletos AS B, Viaje AS V
+                            WHERE B.codigoViaje = V.codigoViaje AND V.numeroVuelo = $vuelo AND V.fecha = '$fechaIng' AND B.fila = $filaA AND B.posicion = '$posicionA' AND B.nombrePasajero = '$nombre' ";
+
+$consultaCodigoViaje = "SELECT DISTINCT V.codigoViaje
+                        FROM Viaje AS V, Rutas AS R
+                        WHERE V.numeroVuelo = R.numeroVuelo AND V.numeroVuelo = $vuelo ";
+
+$ejecutarConsultaAsientoOcupado = pg_query($conexion, $consultaAsientoOcupado);
+
+$ejecutarConsultaCodigoViaje = pg_query($conexion, $consultaCodigoViaje);
 
 
-//$ejecutarConsultaObtenerInfo = pg_query($conexion,$listadoVuelos);
-$consultaInfoVuelos = pg_query($conexion, $infoVuelos);
-
-function resultadosJSON($consultaInfoVuelos, $aerolinea){
-    $info = array();
-    $info['boleto'] = array();
-
-    if (!(pg_num_rows($consultaInfoVuelos))) {
-        echo json_encode(array(''));
-    }else{
-        while ($row = pg_fetch_row($consultaInfoVuelos)) {
-            $vuelo = $row[0];
-            $fecha = $row[1];
-            $hora = $row[2];
-            $numero = $row[3];
-        }
-        //echo json_encode($info);
-        $fechaISO = str_replace('-','', $fecha);
-        $horaISO = str_replace(':', '', DateTime::createFromFormat('H:i:s', $hora)->format('H:i'));
-        $info['boleto'] = array('aerolinea' => $aerolinea, 'vuelo' => $vuelo, 'fecha' => $fechaISO, 'hora' => $horaISO, 'numero' => $numero);
+function resultadosJSON($ejecutarConsultaAsientoOcupado, $ejecutarConsultaCodigoViaje, $aerolinea, $nombre, $filaA, $posicionA, $conexion, $fechaIng, $vuelo){   
     
-        header('Content-Type: application/json');
-        echo json_encode($info);
-    }
+    if (!(pg_num_rows($ejecutarConsultaAsientoOcupado))) {
+        
+        if(!(pg_num_rows($ejecutarConsultaCodigoViaje))){
+            echo "no existe codigoviaje";
+        }else{
+            while ($row = pg_fetch_row($ejecutarConsultaCodigoViaje)) {
+                $codigoViaje = $row[0];
+            }
 
+            $consulta  = sprintf("INSERT INTO Boletos(nombrePasajero, fila, posicion, codigoViaje) VALUES('%s','%s','%s','%s');",
+            pg_escape_string($nombre),
+            pg_escape_string($filaA),
+            pg_escape_string($posicionA),
+            pg_escape_string($codigoViaje)
+            );
+            
+            $ejecutarConsulta = pg_query($conexion, $consulta);
+            $info['boleto'] = array();
+            if ($ejecutarConsulta) {
+                $consultaAsientoOcupado2 = " SELECT V.numeroVuelo, V.fecha, R.horaSalida, B.numeroBoleto
+                            FROM Boletos AS B, Viaje AS V, Rutas AS R
+                            WHERE B.codigoViaje = V.codigoViaje AND R.numeroVuelo = V.numeroVuelo AND V.numeroVuelo = $vuelo AND V.fecha = '$fechaIng' AND B.fila = $filaA AND B.posicion = '$posicionA' AND B.nombrePasajero = '$nombre' ";
+
+                $ejecutarConsultaAsientoOcupado2 = pg_query($conexion, $consultaAsientoOcupado2);
+
+                if(!(pg_num_rows($ejecutarConsultaAsientoOcupado2))){
+                    echo "no existe codigoviaje";
+                }else{
+                    while ($row = pg_fetch_row($ejecutarConsultaAsientoOcupado2)) {
+                        $numeroVuelo = $row[0];
+                        $fecha = $row[1];
+                        $hora = $row[2];
+                        $numero = $row[3];
+                    }
+                    
+                    $fechaISO = str_replace('-','', $fecha);
+                    $horaISO = str_replace(':', '', DateTime::createFromFormat('H:i:s',$hora)->format('H:i'));
+                    $info['boleto'] = array('aerolinea' => $aerolinea, 'vuelo' => $numeroVuelo, 'fecha' => $fechaISO, 'hora' => $horaISO, 'numero'=> $numero);
+    
+                    header('Content-Type: application/json');
+                    echo json_encode($info);
+                }
+            }else{
+                echo "los datos ingresados no se guardaron\n";
+            }
+        }
+    }else{ 
+        header('Content-Type: application/json');
+        echo json_encode(array('El asiento esta ocupado'));
+    }
 }
 
-function resultadosXML($consultaInfoVuelos, $aerolinea){
-        // verificamos que existen registros, sino no dibujamos la tabla
-        if (!(pg_num_rows($consultaInfoVuelos))) {
-            echo '<boleto>No hay informacion</boleto>';
+function resultadosXML($ejecutarConsultaAsientoOcupado, $ejecutarConsultaCodigoViaje, $aerolinea, $nombre, $filaA, $posicionA, $conexion, $fechaIng, $vuelo){
+
+    
+    if (!(pg_num_rows($ejecutarConsultaAsientoOcupado))) {
+        
+        if(!(pg_num_rows($ejecutarConsultaCodigoViaje))){
+            echo "no existe codigoviaje";
         }else{
-            echo '<boleto>';
-            //echo "\t<aerolinea>EY</aerolinea>\n";
-                while ($row= pg_fetch_row($consultaInfoVuelos)) {
-                    $vuelo = $row[0];
-                    $fecha = $row[1];
-                    $hora = $row[2];
-                    $numero = $row[3];
+            while ($row = pg_fetch_row($ejecutarConsultaCodigoViaje)) {
+                $codigoViaje = $row[0];
+            }
 
+            $consulta  = sprintf("INSERT INTO Boletos(nombrePasajero, fila, posicion, codigoViaje) VALUES('%s','%s','%s','%s');",
+            pg_escape_string($nombre),
+            pg_escape_string($filaA),
+            pg_escape_string($posicionA),
+            pg_escape_string($codigoViaje)
+            );
+            
+            $ejecutarConsulta = pg_query($conexion, $consulta);
+            if ($ejecutarConsulta) {
+                //echo "guardados correctamente\n";
+
+                $consultaAsientoOcupado2 = " SELECT V.numeroVuelo, V.fecha, R.horaSalida, B.numeroBoleto
+                            FROM Boletos AS B, Viaje AS V, Rutas AS R
+                            WHERE B.codigoViaje = V.codigoViaje AND R.numeroVuelo = V.numeroVuelo AND V.numeroVuelo = $vuelo AND V.fecha = '$fechaIng' AND B.fila = $filaA AND B.posicion = '$posicionA' AND B.nombrePasajero = '$nombre' ";
+
+                $ejecutarConsultaAsientoOcupado2 = pg_query($conexion, $consultaAsientoOcupado2);
+
+                if(!(pg_num_rows($ejecutarConsultaAsientoOcupado2))){
+                    echo "no existe codigoviaje";
+                }else{
+                    while ($row = pg_fetch_row($ejecutarConsultaAsientoOcupado2)) {
+                        $numeroVuelo = $row[0];
+                        $fecha = $row[1];
+                        $hora = $row[2];
+                        $numero = $row[3];
+                    }
+                    
                     $fechaISO = str_replace('-','', $fecha);
-                    $horaISO = str_replace(':', '', DateTime::createFromFormat('H:i:s', $hora)->format('H:i'));
+                    $horaISO = str_replace(':', '', DateTime::createFromFormat('H:i:s',$hora)->format('H:i'));
 
-                    echo "\t<aerolinea>$aerolinea</aerolinea>\n";
-                    echo "\t\t<vuelo>$vuelo</vuelo>\n";
-                    echo "\t\t<fecha>$fechaISO</fecha>\n";
-                    echo "\t\t<hora>$horaISO</hora>\n";
-                    echo "\t\t<numero>$numero</numero>\n";
-
+                    echo "<boleto>";
+                    echo "<aerolinea>$aerolinea</aerolinea>";
+                    echo "<vuelo>$numeroVuelo</vuelo>";
+                    echo "<fecha>$fechaISO</fecha>";
+                    echo "<hora>$horaISO</hora>";
+                    echo "<numero>$numero</numero>";
+                    echo "</boleto>";
                 }
-            echo "</boleto>";
+            }else{
+                echo "los datos ingresados no se guardaron\n";
+            }
         }
+    }else{ 
+        echo "<boleto>";
+        echo "El asiento esta ocupado";
+        echo "</boleto>";
+    }
 }
 
 if($aerolinea == $aerolineaA){
@@ -97,10 +166,10 @@ if($aerolinea == $aerolineaA){
     
             # Se verifica que el formato es XML del cuarto parametro
             if (strcmp($formatoResultado, $formatoXML) === 0){
-                resultadosXML($consultaInfoVuelos, $aerolinea);
+                resultadosXML($ejecutarConsultaAsientoOcupado, $ejecutarConsultaCodigoViaje, $aerolinea, $nombre, $filaA, $posicionA, $conexion, $fechaIng, $vuelo);
             # Se verifica que el formato es JSON del cuarto parametro
             }else  if (strcmp($formatoResultado, $formatoJSON) === 0){
-                resultadosJSON($consultaInfoVuelos, $aerolinea);
+                resultadosJSON($ejecutarConsultaAsientoOcupado, $ejecutarConsultaCodigoViaje, $aerolinea, $nombre, $filaA, $posicionA, $conexion, $fechaIng, $vuelo);
             }else {
                 # code...
                 echo "El formato ingresado $formatoResultado no se reconoce";
@@ -109,7 +178,7 @@ if($aerolinea == $aerolineaA){
     
         }else {
             // no se agrego ningun formato por default es XML
-            resultadosXML($ejecutarConsultaObtenerInfo,$consultaInfoVuelos);
+            resultadosXML($ejecutarConsultaAsientoOcupado, $ejecutarConsultaCodigoViaje, $aerolinea, $nombre, $filaA, $posicionA, $conexion, $fechaIng, $vuelo);
         }
     
     }else {
